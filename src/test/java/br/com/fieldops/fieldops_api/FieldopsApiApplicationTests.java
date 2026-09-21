7a6fcf4f4c8188bb0aee172de6141f7c33e23db5
ceduardooliveira <c-eduardo94@hotmail.com>
2026-09-21 11:56:00 -0300

    test(PBI-007): adiciona testes unitarios e de integracao para autenticacao

--- src/test/java/br/com/fieldops/fieldops_api/FieldopsApiApplicationTests.java
+++ src/test/java/br/com/fieldops/fieldops_api/FieldopsApiApplicationTests.java
@@ -1,9 +1,10 @@
 package br.com.fieldops.fieldops_api;
 
+import br.com.fieldops.api.FieldopsApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FieldopsApiApplication.class)
 class FieldopsApiApplicationTests {
 
 	@Test
